ffb9b4292cc4dc74b1f65832e77068cf47939b22
Victor <v.samsonov123@gmail.com>
2021-09-17 21:32:11 -0500

    navigation in App.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeScreen = () => {
+
+    return (
+        <div>
+            <text>
+                Hello
+            </text>
+        </div>
+    )
+}
+
+export default HomeScreen
