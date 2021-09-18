2f5bcdfbf9318716b1fd19d0d03b7009b7d88b26
James Brouckman <jbrouckman@gmail.com>
2021-09-18 16:29:12 -0400

    additions to info container and css

--- src/components/InfoContainer.js
+++ src/components/InfoContainer.js
@@ -3,29 +3,61 @@ import "./components.css";
 
 import { Button } from "@mui/material";
 import { message } from "./SideBarData";
+import { black } from "jest-matcher-utils/node_modules/chalk";
 const InfoContainer = (props) => {
   const BUTTON_HEIGHT = 37.5;
   const analysis = message; //props.analysis;
 
-  const getHighlightedText = (text, hightlight) => {
+  const getHighlightedText = (text, keyPhrases, entities) => {
     // Split on highlight term and include term into parts, ignore case
-    let regex = "(" + hightlight.join(`) | (`) + ")";
+    let regex = "(" + keyPhrases.join(`) | (`) + ")";
+    let names = [];
+    Array.prototype.forEach.call(entities, (entity) => {
+      regex = regex + `| (${entity.name})`;
+      names.push(entity.name);
+    });
+    console.log(regex);
     const parts = text.split(new RegExp(regex, "gi"));
+
     return (
       <span>
         {" "}
-        {parts.map((part, i) => (
-          <span
-            key={i}
-            style={
-              hightlight.indexOf(part) > -1
-                ? { fontWeight: "bold", backgroundColor: "cyan" }
-                : {}
-            }
-          >
-            {part}
-          </span>
-        ))}{" "}
+        {parts.map((part, i) => {
+          let style = {};
+          if (keyPhrases.indexOf(part) > -1) {
+            style = {
+              //fontWeight: "bold",
+              color: "red",
+              textDecoration: "underline",
+            };
+          }
+          if (names.indexOf(part) > -1) {
+            style = {
+              fontWeight: "bold",
+            };
+            return (
+              <span key={i} style={style}>
+                <a
+                  style={{
+                    color: "blue",
+                    textDecoration: "none",
+                    fontStyle: "italic",
+                  }}
+                  href={entities[names.indexOf(part)].url}
+                  target="_blank"
+                >
+                  {part + " "}
+                </a>
+              </span>
+            );
+          }
+          if (part != undefined)
+            return (
+              <span key={i} style={style}>
+                {part + " "}
+              </span>
+            );
+        })}{" "}
       </span>
     );
   };
@@ -33,7 +65,13 @@ const InfoContainer = (props) => {
   console.log(message.sentences instanceof Array);
   Array.prototype.forEach.call(message.sentences, (sentence) => {
     textObjects.push(
-      <p>{getHighlightedText(sentence.text, analysis.key_phrases)} </p>
+      <p>
+        {getHighlightedText(
+          sentence.text,
+          analysis.key_phrases,
+          analysis.entities
+        )}{" "}
+      </p>
     );
   });
 
