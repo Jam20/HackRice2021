53368d6a8f3c545084d44617a7a5f60c9b71fea7
James Brouckman <jbrouckman@gmail.com>
2021-09-18 17:51:24 -0400

    Adjusted styles for text display elements

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,75 +1,55 @@
-import React, {useState} from 'react'
-import './screens.css'
-import SideMenu from '../components/SideMenu'
-import InfoContainer from '../components/InfoContainer'
-import { Button } from '@mui/material'
-import { useHistory } from 'react-router-dom'
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from "react";
+import "./screens.css";
+import SideMenu from "../components/SideMenu";
+import InfoContainer from "../components/InfoContainer";
+import { Button } from "@mui/material";
+import { useHistory } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 const MainScreen = (props) => {
-    const history = useHistory()
-    const BUTTON_HEIGHT = 37.5;
-    const [sidebar, setSidebar] = useState(true)
-    const pressSideBar = () =>{
-      setSidebar(!sidebar)
-    }
-    /**prop analysis will give the current analysis to be displayed on the page. follows same format as python analysis object */
-    
-    return (
-        <>
-         <div className='top-bar'>
-            <text class="title-text" onClick={()=>history.push('/home')}>
-              Summarizer
-            </text>
-            
-            <Button
-              sx={{
-                background:'#1db954',
-                padding:1,
-                paddingRight:6,
-                paddingLeft:6,
-                height:BUTTON_HEIGHT,
-                marginLeft:'auto',
-                marginRight:'15%',
-                marginTop:'auto',
-                marginBottom:'auto',
-                '&:hover': {
-                  opacity:.7,
-                  background:'#1db954',
-                },
-              }}
-              onClick={() => history.push('/home')}
-            >
-              <text className={'button-text'}>
-                Export
-              </text>
-            </Button>
-          </div>
-          <div className='container'>
-                <div className='wrapper'>
-                
-                  <div className={
-                    sidebar ?'left-section' : 'sidebar-not-visible'
-                    }>
-                    <SideMenu/>
-                  </div>
-                  <a onClick={pressSideBar}>
-                    <MenuIcon
-                      sx={{
-                        color:'white',
-                        width:30,
-                        height:30,
-                        marginLeft:2,
-                        marginRight:2,
-                      }}
-                    />
-                  </a>
-                  <div className="middle-right-section">
-                    <InfoContainer/>
-                  </div>
-              </div>
-          </div>
-        </>
-    )
-}
+  const history = useHistory();
+  const BUTTON_HEIGHT = 37.5;
+  /**prop analysis will give the current analysis to be displayed on the page. follows same format as python analysis object */
 
-export default MainScreen
+  return (
+    <>
+      <div className="top-bar">
+        <text class="title-text" onClick={() => history.push("/home")}>
+          Summarizer
+        </text>
+
+        <Button
+          sx={{
+            background: "#1db954",
+            padding: 1,
+            paddingRight: 6,
+            paddingLeft: 6,
+            height: BUTTON_HEIGHT,
+            marginLeft: "auto",
+            marginRight: "3%",
+            marginTop: "auto",
+            marginBottom: "auto",
+            "&:hover": {
+              opacity: 0.7,
+              background: "#1db954",
+            },
+          }}
+          onClick={() => history.push("/home")}
+        >
+          <text className={"button-text"}>Export</text>
+        </Button>
+      </div>
+      <div className="container">
+        <div className="wrapper">
+          <div>
+            <SideMenu />
+          </div>
+          <div className="middle-right-section">
+            <InfoContainer />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainScreen;
